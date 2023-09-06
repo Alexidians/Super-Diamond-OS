5b20dcb0815bc4e376c1b9e22f8c0a0649425322
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-09-06 20:53:24 +0300

    Update templates.js

--- templates.js
+++ templates.js
@@ -1,3 +1,5 @@
+//for all unsupported languages use a converter we put urls to a converter
+
 //adds a lua script file to OS
 var luaScript = document.createElement("script")
 luaScript.src = "lua script file url"
