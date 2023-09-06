83fcc18bdff740a8d0705a6a90e6f00c652347c9
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-09-06 20:47:06 +0300

    Update templates.js

--- templates.js
+++ templates.js
@@ -2,3 +2,5 @@
 var luaScript = document.createElement("script")
 luaScript.src = "lua script file url"
 document.body.appendChild(luaScript)
+
+//to use r script convert it to js using this https://www.codeconvert.ai/r-to-javascript-converter
