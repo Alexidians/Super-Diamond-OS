28223a53c24d911b68d86fa365dbafddbd6a6fcb
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-09-09 08:07:47 +0300

    Delete framed-app-proccesor.js

--- framed-app-proccesor.js
+++
@@ -1,7 +0,0 @@
-var OS = "addded on import"
-
-/*import code
-var proccesorImporter = appFrameVar.contentWindow.document.createElement("script")
-proccesorImporter.src = "https://alexidians.github.io/Super-Diamond-OS/framed-app-proccesor.js"
-appFrameVar.contentWindow.document.body.appendChild(proccesorImporter)
-appFrameVar.contentWindow.var OS = window
