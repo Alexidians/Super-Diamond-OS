270abf9a9e9de401bd5dfb66fca6f8a136a0caf9
Alexidians <142623358+Alexidians@users.noreply.github.com>
2023-09-09 08:05:53 +0300

    Update framed-app-proccesor.js

--- framed-app-proccesor.js
+++ framed-app-proccesor.js
@@ -1,4 +1,7 @@
-var osVar = "addded on import"
-var OS = function execute(function) {
- return osVar.eval(function);
-}
+var OS = "addded on import"
+
+/*import code
+var proccesorImporter = appFrameVar.contentWindow.document.createElement("script")
+proccesorImporter.src = "https://alexidians.github.io/Super-Diamond-OS/framed-app-proccesor.js"
+appFrameVar.contentWindow.document.body.appendChild(proccesorImporter)
+appFrameVar.contentWindow.var OS = window
