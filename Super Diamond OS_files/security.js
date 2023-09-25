43a750ed419e55e61fb876ceedc2c3ce260863da
Alex <142623358+Alexidians@users.noreply.github.com>
2023-09-25 16:51:35 +0300

    Update security.js

--- Super Diamond OS_files/security.js
+++ Super Diamond OS_files/security.js
@@ -1,6 +1,6 @@
 securityCheck()
 function securityCheck() {
  if(window.self !== window.top) {
-  document.write("Security Firewall Activated. Reason: We do not allow this webpage to be embeded in an iframe")
+  document.body.innerHTML = "Security Firewall Activated. Reason: We do not allow this webpage to be embeded in an iframe"
  }
 }
