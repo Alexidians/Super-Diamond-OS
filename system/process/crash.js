54619c0c5b9c2243e0f43a04860442da4bbd1c67
Alex <142623358+Alexidians@users.noreply.github.com>
2023-12-28 20:39:09 +0200

    Update crash.js

--- system/process/crash.js
+++ system/process/crash.js
@@ -1,23 +1,3 @@
-    function Crash(crash) {
-     crashInterval = setInterval(CrashAct, 0, crash)
-     setTimeout(CrashMiddle, 5000)
-    }
-    function CrashMiddle() {
-     clearInterval(crashInterval)
-     document.getElementById("desktop").remove()
-     var png = document.createElement("img")
-     png.src = "pc-error.png"
-     png.width = "2280"
-     png.height = "1500"
-     png.id = "CrashPng"
-     document.body.appendChild(png)
-     setTimeout(CrashEnd, 10000)
-    }
-    function CrashEnd() {
-     document.getElementById("CrashPng").remove()
-     document.body.style.backgroundColor = "black";
-    }
-    function CrashAct(error) {
-     ErrorMessage(error)
-     WarningMessage(error)
-    }
+window.onerror = function (message, source, lineno, colno, error) {
+  location.replace("bluescreen.html?message=" + encodeURI(message) + "&source=" + encodeURI(source) + "&lineno=" + encodeURI(lineno) + "&colno=" + encodeURI(colno) + "&error" + encodeURI(error))
+};
