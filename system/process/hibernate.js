8cd7d81524aebda433ec67aaf8e895a2a7d3b637
Alex <142623358+Alexidians@users.noreply.github.com>
2023-12-28 20:25:09 +0200

    Create hibernate.js

---
+++ system/process/hibernate.js
@@ -0,0 +1,90 @@
+function hibernate() {
+    const appsDiv = document.getElementById('Apps');
+    const TextRAMDiv = document.getElementById('TextRAM');
+    const ScriptRAMDiv = document.getElementById('ScriptRAM');
+    const AppRAMArray = [];
+    const TextRAMArray = [];
+    const ScriptRAMArray = [];
+    
+    Array.from(appsDiv.children).forEach(application => {
+        AppRAMArray.push({
+            id: application.id,
+            content: application.innerHTML,
+            windowWidth: application.style.width,
+            windowHeight: application.style.height
+        });
+    });
+    Array.from(TextRAMDiv.children).forEach(data => {
+        TextRAMArray.push({
+            name: data.getAttribute("name"),
+            value: data.getAttribute("value"),
+        });
+    });
+    Array.from(ScriptRAMDiv.children).forEach(script => {
+        ScriptRAMArray.push({
+            code: script.text,
+        });
+    });
+
+    localStorage.setItem('AppRAM', JSON.stringify(AppRAMArray));
+    localStorage.setItem('TextRAM', JSON.stringify(TextRAMArray));
+    localStorage.setItem('ScriptRAM', JSON.stringify(ScriptRAMArray));
+    localStorage.setItem('washibernated', 'true');
+    ShutDown()
+}
+
+function loadRAMFromHibernation() {
+
+    if (localStorage.getItem('washibernated') === 'true') {
+        const AppRAMArray = JSON.parse(localStorage.getItem('AppRAM'));
+        const TextRAMArray = JSON.parse(localStorage.getItem('TextRAM'));
+        const ScriptRAMArray = JSON.parse(localStorage.getItem('ScriptRAM'));
+        const appsDiv = document.getElementById('Apps');
+        const TextRAMDiv = document.getElementById('TextRAM');
+        const ScriptRAMDiv = document.getElementById('ScriptRAM');
+
+        appsDiv.innerHTML = ""
+        AppRAMArray.forEach(application => {
+            const App = document.createElement('div');
+            App.class = "window";
+            App.style.width = application.windowWidth;
+            App.style.height = application.windowHeight;
+            App.style.backgroundColor = "#fff";
+            App.style.border = "1px solid #ccc";
+            App.style.position = "absolute";
+            App.style.top = "100px";
+            App.style.left = "100px";
+            App.style.cursor = "move";
+            App.id = application.id;
+            App.innerHTML = application.content;
+            appsDiv.appendChild(App);
+            makeDraggable(App)
+            const scripts = App.querySelectorAll('script');
+            scripts.forEach(script => {
+             const newScript = document.createElement('script');
+             newScript.text = script.text;
+             document.head.appendChild(newScript)
+             document.head.removeChild(newScript);
+            });
+        });
+
+        TextRAMDiv.innerHTML = ""
+        TextRAMArray.forEach(data => {
+            const Elem = document.createElement('data');
+            Elem.setAttribute("name", data.name)
+            Elem.setAttribute("value", data.value)
+            TextRAMDiv.appendChild(Elem)
+        });
+
+        ScriptRAMDiv.innerHTML = ""
+        ScriptRAMArray.forEach(script => {
+            const Elem = document.createElement('script');
+            Elem.innerHTML = script.code
+            ScriptRAMDiv.appendChild(Elem)
+        });
+    }
+    localStorage.setItem('washibernated', 'false');
+}
+
+
+window.addEventListener('load', loadRAMFromHibernation);
