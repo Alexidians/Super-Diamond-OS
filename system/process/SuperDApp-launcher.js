8c8b9c81d48d02189f018986cdbda0d10da51c13
Alex <142623358+Alexidians@users.noreply.github.com>
2023-12-29 20:59:51 +0200

    Create SuperDApp-launcher.js

---
+++ system/process/SuperDApp-launcher.js
@@ -0,0 +1,43 @@
+function execSuperDApp(content) {
+ var manifest = JSON.parse(atob(content))
+ if(manifest.window.exsists == true) {
+  var App = document.createElement("div")
+  var AppTitle = document.createElement("h3")
+  App.class = "window"
+  App.style.width = "600px";
+  App.style.height = "500px";
+  App.style.backgroundColor = "#fff";
+  App.style.border = "1px solid #ccc";
+  App.style.position = "absolute";
+  App.style.top = "100px";
+  App.style.left = "100px";
+  App.style.cursor = "move";
+  AppTitle.innerHTML = manifest.name;
+  App.id = manifest.name
+  if(manifest.window.type == "iframe") {
+   var AppFrame = document.createElement("iframe")
+   AppTitle.InnerHTML = manifest.name
+   AppTitle.appendChild(AppTitleTextNode)
+   App.appendChild(AppTitle)
+   App.appendChild(AppFrame)
+   AppFrame.src = manifest.window.url
+   AppFrame.width = "600"
+   AppFrame.height = "500"
+   document.getElementById("Apps").appendChild(App)
+   makeDraggable(App)
+  }
+  if(manifest.window.type == "html") {
+   var AppFrame = document.createElement("iframe")
+   AppTitle.InnerHTML = manifest.name
+   AppTitle.appendChild(AppTitleTextNode)
+   App.appendChild(AppTitle)
+   App.appendChild(AppFrame)
+   AppFrame.srcdoc = manifest.window.html
+   AppFrame.width = "600"
+   AppFrame.height = "500"
+   document.getElementById("Apps").appendChild(App)
+   makeDraggable(App)
+  }
+ }
+ eval(manifest.code)
+}
