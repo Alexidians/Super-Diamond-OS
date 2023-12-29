a4a185a6a3e84a8a54f1d88bc3f3995958dffc75
Alex <142623358+Alexidians@users.noreply.github.com>
2023-12-29 13:42:28 +0200

    Update crash.js

--- system/process/crash.js
+++ system/process/crash.js
@@ -1,3 +1,3 @@
 window.onerror = function (message, source, lineno, colno, error) {
-  location.replace("bluescreen.html?message=" + encodeURI(message) + "&source=" + encodeURI(source) + "&lineno=" + encodeURI(lineno) + "&colno=" + encodeURI(colno) + "&error" + encodeURI(error))
+  location.replace("bluescreen.html?message=" + encodeURI(message) + "&source=" + encodeURI(source) + "&lineno=" + encodeURI(lineno) + "&colno=" + encodeURI(colno) + "&error" + encodeURI(error.toString()) + "&stack=" + encodeURI(error.stack))
 };
