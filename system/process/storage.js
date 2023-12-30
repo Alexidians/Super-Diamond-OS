44bef1435cdb7c5a3d1151c1e136aa497cd93486
Alex <142623358+Alexidians@users.noreply.github.com>
2023-12-30 21:40:20 +0200

    Update storage.js

--- system/process/storage.js
+++ system/process/storage.js
@@ -35,4 +35,13 @@
       }
     }
 
-    const SDStorage = new SuperDiamondOSDatabase();
+    const SDStorageDB = new SuperDiamondOSDatabase();
+    async function deleteSystemItem(key) => {
+      await SDStorageDB.deleteSystemItem(key);
+    };
+    async function getSystemItem(key) => {
+      return await SDStorageDB.getSystemItem(key);
+    };
+    async function setSystemItem(key, value) => {
+      await SDStorageDB.setSystemItem(key, value);
+    };
