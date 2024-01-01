d59e0fdc8ebd25a131d5c0721eb747eb2481f1bf
Alex <142623358+Alexidians@users.noreply.github.com>
2024-01-01 19:26:05 +0200

    Update graphics.js

--- system/process/graphics.js
+++ system/process/graphics.js
@@ -3,15 +3,3 @@ var graphics = {
   return SuperDiamondOSErrors.graphicsError(msg);
  }
 }
-var SuperDiamondOSErrors = {
- graphicsError: function(msg) {
-  var Err = new Error(msg)
-  Err.name = "graphicsError"
-  Err.code = "GRAPHICS_ERR"
-  Err.message = "graphicsError: " + msg
-  Err.lineNumber = NaN
-  Err.columnNumber = NaN
-  Err.fileName = "https://alexidians.github.io/Super-Diamond-OS/system/process/graphics.js"
-  return Err;
- }
-}
