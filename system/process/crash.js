aaa1bbd2bb3a6f1bc6b5a8b06b8e143c0bcc51b3
Alex <142623358+Alexidians@users.noreply.github.com>
2024-01-01 19:24:02 +0200

    Update crash.js

--- system/process/crash.js
+++ system/process/crash.js
@@ -1,3 +1,16 @@
 window.onerror = function (message, source, lineno, colno, error) {
   location.replace("bluescreen.html?message=" + encodeURI(message) + "&source=" + encodeURI(source) + "&lineno=" + encodeURI(lineno) + "&colno=" + encodeURI(colno) + "&error" + encodeURI(error.toString()) + "&stack=" + encodeURI(error.stack))
 };
+
+var SuperDiamondOSErrors = {
+ graphicsError: function(msg) {
+  var Err = new Error(msg)
+  Err.name = "graphicsError"
+  Err.code = "GRAPHICS_ERR"
+  Err.message = "graphicsError: " + msg
+  Err.lineNumber = NaN
+  Err.columnNumber = NaN
+  Err.fileName = "https://alexidians.github.io/Super-Diamond-OS/system/process/graphics.js"
+  return Err;
+ }
+}
