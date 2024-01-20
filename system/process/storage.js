f67165ed3b9aa0ad430faf86c2f11947d52be685
Alex <142623358+Alexidians@users.noreply.github.com>
2024-01-20 19:14:00 +0200

    Update storage.js

--- system/process/storage.js
+++ system/process/storage.js
@@ -1,50 +1,74 @@
-    class SuperDiamondOSDatabase extends Dexie {
-      constructor() {
-        super('SUPER_DIAMOND_OS');
-        this.version(1).stores({
-          system: '++id, key, value',
-        });
-      }
-
-      async setSystemItem(key, value) {
-       try {
-        const existingItem = await this.system.where({ key }).first();
-        if (existingItem) {
-         await this.system.update(existingItem.id, { value });
-        } else {
-         await this.system.add({ key, value });
-        }
-       } catch (error) {
-        console.error('Error setting system item:', error);
-       }
-      };
-
-      async getSystemItem(key) {
-        try {
-          const result = await this.system.where({ key }).first();
-          return result ? result.value : null;
-        } catch (error) {
-          console.error('Error getting system item:', error);
-          return null;
-        }
-      }
-
-      async deleteSystemItem(key) {
-        try {
-          await this.system.where({ key }).delete();
-        } catch (error) {
-          console.error('Error deleting system item:', error);
-        }
-      }
+var Storage = {
+ System: {
+  set: async function(key, value) {
+   return new Promise((resolve, reject) => {
+    try {
+     await Storage.db.SystemStorage.put(value, key);
+     resolve({status: "sucess", data: {name: key, value: value}});
+    } catch(err) {
+     resolve({status: "failed", data: err});
     }
-
-    const SDStorageDB = new SuperDiamondOSDatabase();
-    async function deleteSystemItem(key) {
-      await SDStorageDB.deleteSystemItem(key);
-    };
-    async function getSystemItem(key) {
-      return await SDStorageDB.getSystemItem(key);
-    };
-    async function setSystemItem(key, value) {
-      await SDStorageDB.setSystemItem(key, value);
-    };
+   });
+  },
+  get: async function(key) {
+   return new Promise((resolve, reject) => {
+    try {
+     var value = await Storage.db.SystemStorage.get(key);
+     resolve({status: "sucess", data: {name: key, value: value}});
+    } catch(err) {
+     resolve({status: "failed", data: err});
+    }
+   });
+  }
+ },
+ App: {
+  set: async function(key, value) {
+   return new Promise((resolve, reject) => {
+    try {
+     await Storage.db.AppStorage.put(value, key);
+     resolve({status: "sucess", data: {name: key, value: value}});
+    } catch(err) {
+     resolve({status: "failed", data: err});
+    }
+   });
+  },
+  get: async function(key) {
+   return new Promise((resolve, reject) => {
+    try {
+     var value = await Storage.db.AppStorage.get(key);
+     resolve({status: "sucess", data: {name: key, value: value}});
+    } catch(err) {
+     resolve({status: "failed", data: err});
+    }
+   });
+  }
+ },
+ Mod: {
+  set: async function(key, value) {
+   return new Promise((resolve, reject) => {
+    try {
+     await Storage.db.ModStorage.put(value, key);
+     resolve({status: "sucess", data: {name: key, value: value}});
+    } catch(err) {
+     resolve({status: "failed", data: err});
+    }
+   });
+  },
+  get: async function(key) {
+   return new Promise((resolve, reject) => {
+    try {
+     var value = await Storage.db.ModStorage.get(key);
+     resolve({status: "sucess", data: {name: key, value: value}});
+    } catch(err) {
+     resolve({status: "failed", data: err});
+    }
+   });
+  }
+ }
+}
+Storage.db = new Dexie("SUPER_DIAMOND_OS");
+Storage.db.version(1).stores({
+	SystemStorage: '',
+    AppStorage: '',
+    ModStorage: ''
+});
