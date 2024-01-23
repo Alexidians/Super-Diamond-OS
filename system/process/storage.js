b8674a28d5fab660fdd19bc0a0a1cc1936669ee6
Alex <142623358+Alexidians@users.noreply.github.com>
2024-01-23 16:40:49 +0200

    Update storage.js

--- system/process/storage.js
+++ system/process/storage.js
@@ -1,74 +1,10 @@
 var Storage = {
- System: {
-  set: async function(key, value) {
-   return new Promise((resolve, reject) => {
-    try {
-     await Storage.db.SystemStorage.put(value, key);
-     resolve({status: "sucess", data: {name: key, value: value}});
-    } catch(err) {
-     resolve({status: "failed", data: err});
-    }
-   });
-  },
-  get: async function(key) {
-   return new Promise((resolve, reject) => {
-    try {
-     var value = await Storage.db.SystemStorage.get(key);
-     resolve({status: "sucess", data: {name: key, value: value}});
-    } catch(err) {
-     resolve({status: "failed", data: err});
-    }
-   });
-  }
+ setItem: async function(name, value) {
+  var value = await localforage.setItem(name, value);
+  return value;
  },
- App: {
-  set: async function(key, value) {
-   return new Promise((resolve, reject) => {
-    try {
-     await Storage.db.AppStorage.put(value, key);
-     resolve({status: "sucess", data: {name: key, value: value}});
-    } catch(err) {
-     resolve({status: "failed", data: err});
-    }
-   });
-  },
-  get: async function(key) {
-   return new Promise((resolve, reject) => {
-    try {
-     var value = await Storage.db.AppStorage.get(key);
-     resolve({status: "sucess", data: {name: key, value: value}});
-    } catch(err) {
-     resolve({status: "failed", data: err});
-    }
-   });
-  }
- },
- Mod: {
-  set: async function(key, value) {
-   return new Promise((resolve, reject) => {
-    try {
-     await Storage.db.ModStorage.put(value, key);
-     resolve({status: "sucess", data: {name: key, value: value}});
-    } catch(err) {
-     resolve({status: "failed", data: err});
-    }
-   });
-  },
-  get: async function(key) {
-   return new Promise((resolve, reject) => {
-    try {
-     var value = await Storage.db.ModStorage.get(key);
-     resolve({status: "sucess", data: {name: key, value: value}});
-    } catch(err) {
-     resolve({status: "failed", data: err});
-    }
-   });
-  }
+ getItem: async function(name) {
+  var value = await localforage.setItem(name);
+  return value;
  }
 }
-Storage.db = new Dexie("SUPER_DIAMOND_OS");
-Storage.db.version(1).stores({
-	SystemStorage: '',
-    AppStorage: '',
-    ModStorage: ''
-});
