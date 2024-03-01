76c823e7a2c962c1b048d8fda165eb0c7572b688
Alex <142623358+Alexidians@users.noreply.github.com>
2024-03-01 16:43:23 +0200

    Update emailJSLoader.js

--- system/process/emailJSLoader.js
+++ system/process/emailJSLoader.js
@@ -1,3 +1,4 @@
+LogWriteInfo("Starting Task: system/process/emailJSLoader.js")
 LogWriteInfo("Initalizing EmailJS Super Diamond Community")
 emailjs.init("9EOT4CjiLi7_ipO_l");
 LogWriteInfo("Sucesfuly initalized EmailJS Super Diamond Community")
