2afcf1ab7d3f14c83f23ce74c0042dd40b12f9e2
Alex <142623358+Alexidians@users.noreply.github.com>
2024-03-01 16:29:48 +0200

    Update app-code-executor.js

--- system/process/app-code-executor.js
+++ system/process/app-code-executor.js
@@ -1,6 +1,10 @@
-    window.addEventListener("message", function (e) {
+LogWriteInfo("Starting Task: system/process/app-code-executer.js")
+LogWriteInfo("initalizing iframe window application os interacrion environment")
+window.addEventListener("message", function (e) {
      if(new URL(e.origin).protocol !== "https:" && new URL(e.origin).protocol !== "file:") {
        return;
      }
      eval(e.data)
     })
+LogWriteInfo("iframe window application os interacrion environment initalized sucesfully")
+LogWriteInfo("listening for iframe window application os interacrion requests")
