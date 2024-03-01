32daec159c622997dc094c2a214a05cf054550d8
Alex <142623358+Alexidians@users.noreply.github.com>
2024-03-01 16:45:10 +0200

    Update graphics.js

--- system/process/graphics.js
+++ system/process/graphics.js
@@ -1,5 +1,10 @@
+LogWriteInfo("Starting Task: system/process/graphics.js")
+LogWriteInfo("initalizing graphics object")
 var graphics = {
  Error: function(msg) {
+  LogWriteInfo("initalizing graphics error object")
   return SuperDiamondOSErrors.graphicsError(msg);
  }
 }
+LogWriteInfo("initalized graphics object sucesfully")
+LogWriteInfo("Listening for graphics object requests")
