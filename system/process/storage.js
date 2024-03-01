a3cf756c0103c78eb6e68c5c8be68590f60b5bf1
Alex <142623358+Alexidians@users.noreply.github.com>
2024-03-01 20:38:33 +0200

    Update storage.js

--- system/process/storage.js
+++ system/process/storage.js
@@ -1,10 +1,16 @@
+LogWriteInfo("Starting Task: system/process/storage.js")
+LogWriteInfo("Initalizing Storage Object")
 var Storage = {
  setItem: async function(name, value) {
+  LogWriteInfo("setting storage key " + name + " value to " + value)
   var value = await localforage.setItem(name, value);
   return value;
  },
  getItem: async function(name) {
+  LogWriteInfo("Getting Storage key " + name + " value")
   var value = await localforage.setItem(name);
   return value;
  }
 }
+LogWriteInfo("Storage Object initalized Sucesfully")
+LogWriteInfo("Now listening for storage requests")
