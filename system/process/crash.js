0f0ea544457c6f5b25cefebb2007622da624b83d
Alex <142623358+Alexidians@users.noreply.github.com>
2025-05-16 16:51:05 +0300

    Update crash.js

--- system/process/crash.js
+++ system/process/crash.js
@@ -1,9 +1,13 @@
 LogWriteInfo("Starting Task: system/process/crash.js")
 LogWriteInfo("initalizing bluescreen error catcher")
 window.onerror = function (message, source, lineno, colno, error) {
-  LogWriteInfo("initalizing bluescreen")
-  LogWriteFatalERR("bluescreen detected: message: " + message + ", source: " + source + ", lineno: " + lineno + ", colno: " + colno + ", error: " + error.toString() + ", stack: " + error.stack)
-  location.replace("bluescreen.html?message=" + encodeURI(message) + "&source=" + encodeURI(source) + "&lineno=" + encodeURI(lineno) + "&colno=" + encodeURI(colno) + "&error" + encodeURI(error.toString()) + "&stack=" + encodeURI(error.stack))
+  LogWriteInfo("initalizing bluescreen");
+  LogWriteFatalERR("bluescreen detected: message: " + message + ", source: " + source + ", lineno: " + lineno + ", colno: " + colno + ", error: " + error.toString() + ", stack: " + error.stack);
+  if(localStorage.getItem("disable_bluescreen") == "true") {
+    LogWriteWarn("Bluescreen Is Disabled. Not showing bluescreen.");
+    return;
+  }
+  location.replace("bluescreen.html?message=" + encodeURI(message) + "&source=" + encodeURI(source) + "&lineno=" + encodeURI(lineno) + "&colno=" + encodeURI(colno) + "&error" + encodeURI(error.toString()) + "&stack=" + encodeURI(error.stack));
 };
 LogWriteInfo("now listening for bluescreens")
 LogWriteInfo("initalizing SuperDiamondOSErrors object")
